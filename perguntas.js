0d63bca4b960470873b93ae63454e2babae9d6b0
RyanGPS917 <ryan.salazar@escola.pr.gov.br>
2025-11-06 14:08:21 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é tecnologia ?',
+    'conjunto de técnicas, métodos, ferramentas e processos usados para resolver problemas e atingir objetivos, aplicando o conhecimento científico e prático para criar soluções que melhoram a vida das pessoas e da sociedade'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Tecnilogia',
+    'o que é automação industrial?',
+    ' o uso de tecnologias como softwares, hardwares, robôs e sistemas de controle para otimizar ou substituir processos manuais em ambientes de produção'
+)
+criaCartao(
+    'Teconologia',
+    'o que é IA?',
+    'é um campo da computação que permite que máquinas simulem habilidades humanas como aprendizado, raciocínio, resolução de problemas e tomada de decisões'
+)
+criaCartao(
+    'Tecnologia',
+    'O que significa IA?',
+    'inteligência artificial '
+)
